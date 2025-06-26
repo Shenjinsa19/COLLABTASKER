0be2bfcb66e1270320cc0c4b7b9fe901e4276708
Shenjinsa19 <shenjinsolomon@gmail.com>
2025-06-26 15:44:39 +0530

    Initial commit

--- collabtasker/celery.py
+++ collabtasker/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 import ssl
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collabtasker.settings')
-
+CELERY_BROKER_URL = os.getenv('REDIS_URL')
 app = Celery('collabtasker')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
